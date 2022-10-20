2fc36ff40a79f0827f025fa67f5ce4933f2cf0e6
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2022-10-20 01:59:06 -0500

    Prueba

--- DB/dbo/Tables/TiposOperaciones.sql
+++ DB/dbo/Tables/TiposOperaciones.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[TiposOperaciones] (
     [Id]          INT           NOT NULL,
     [Descripcion] NVARCHAR (50) NOT NULL,
+    [Prueba]      INT           NULL,
     CONSTRAINT [PK_TiposOperaciones] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
