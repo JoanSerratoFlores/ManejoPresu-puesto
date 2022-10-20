93cdbe66b29736edbe8b75f1abeb2386ec081379
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2022-10-20 00:59:31 -0500

    Eliminar Security

--- DB/Security/ManejoPresupuestoApp_1.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [ManejoPresupuestoApp]
-    WITH PASSWORD = N'|pqyvvpmrp?&jvxy%Tg{ms|jmsFT7_&#$!~<rbvOxYFsjisR', SID = 0xC1C08F9901EFC64AB49E1632B67EAA52, DEFAULT_DATABASE = [ManejoPresupuesto], DEFAULT_LANGUAGE = [Español], CHECK_POLICY = OFF;
-
